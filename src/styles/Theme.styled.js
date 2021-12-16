f4f81d9841f69158deb788de113ae7ac534928ae
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-12-16 22:04:19 +0500

    Changes some line color

--- src/styles/Theme.styled.js
+++ src/styles/Theme.styled.js
@@ -1,13 +1,13 @@
 export const lightTheme = {
   body: "#FFC61B",
   text: "#000000",
-  bodyRgba: "252, 246, 244",
+  bodyRgba: "#e3a765",
   textRgba: "0,0,0"
 };
 
 export const darkTheme = {
   body: "#000000",
   text: "#FFC61B",
-  textRgba: "252, 246, 244",
+  textRgba: "#e3a765",
   bodyRgba: "0,0,0"
 };
