ab8368fa2bd30098337f90061b314dbcebec7a84
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-12-19 22:29:50 +0500

    Completed and updated component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,123 @@
-import React from 'react'
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "../styles/Theme.styled";
+import Button from "./additional/Button";
+import { BackEnd, FrontEnd } from "./additional/Img/svg";
+import Logo from "./additional/Logo";
+import PacrticleComponent from "./additional/PacrticleComponent";
+import SocialMedia from "./additional/SocialMedia";
 
+const Box = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  background: ${(props) => props.theme.body};
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+const Main = styled.div`
+  width: 30vw;
+  height: 60vh;
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  background: ${(props) => props.theme.body};
+  z-index: 3;
+  padding: 2rem;
+  line-height: 1.5;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-family: "Ubuntu Mono", monospace;
+
+  &:hover {
+    color: ${(props) => props.theme.body};
+    background-color: ${(props) => props.theme.text};
+  }
+`;
+const Title = styled.h2`
+  font-size: calc(1em + 1vw);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.5;
+
+  ${Main}:hover & {
+    & > * {
+      fill: ${(props) => props.theme.body};
+    }
+  }
+  & > *:first-child {
+    margin-right: 1rem;
+  }
+`;
+
+const Description = styled.div`
+  font-size: calc(0.6em + 1vw);
+  color: ${(props) => props.theme.text};
+  padding: 0.5rem 0;
+
+  strong {
+    text-transform: uppercase;
+  }
+
+  p {
+    margin-left: 2rem;
+  }
+
+  ${Main}:hover & {
+    color: ${(props) => props.theme.body};
+    background-color: ${(props) => props.theme.text};
+  }
+`;
 const Skills = () => {
-    return (
-        <div>
-            asdsad
-        </div>
-    )
-}
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box>
+        <Logo theme='darkTheme'/>
+        <SocialMedia theme='darkTheme'/>
+        <Button />
+        <PacrticleComponent theme="darkTheme" />
+        <Main>
+          <Title>
+            <FrontEnd width={70} height={70} />
+            Front End Developer
+          </Title>
+          <Description>
+            I am a frontend developer. I have been practicing this field for
+            over a year
+          </Description>
+          <Description>
+            <strong>Skills:</strong>
+            <p>Html, Css, Javascript, React, Bootstrap etc.</p>
+          </Description>
+          <Description>
+            <strong>Tools:</strong>
+            <p>VScode, Github, Stackoverflow, You Tube, Google</p>
+          </Description>
+        </Main>
+        <Main>
+          <Title>
+            <BackEnd width={70} height={70} />
+            Back End Developer
+          </Title>
+          <Description>
+            I am also currently studying the Backend field.
+          </Description>
+          <Description>
+            <strong>Studying resource:</strong>
+            <p>Udemy cource, You Tube, Dadajanov's lessons and others.</p>
+          </Description>
+          <Description>
+            <strong>Tools:</strong>
+            <p>Node js, Express js, MongoDb and others.</p>
+          </Description>
+        </Main>
+      </Box>
+    </ThemeProvider>
+  );
+};
 
-export default Skills
+export default Skills;
