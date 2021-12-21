c9cec0a4807d27feb90c0b160efa4eb8e91394dc
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-12-21 23:02:27 +0500

    Added Card compoent

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,41 +1,80 @@
-import styled, { keyframes } from "styled-components";
-import React from "react";
-import { ThemeProvider }  from "styled-components";
+import styled from "styled-components";
+import React, { useEffect, useRef } from "react";
+import { ThemeProvider } from "styled-components";
 import { darkTheme } from "../styles/Theme.styled";
 import Button from "./additional/Button";
 import Logo from "./additional/Logo";
 import SocialMedia from "./additional/SocialMedia";
 
-import card from './assets/card.png'
+import WorkData from "./data/workData";
+import Card from "./additional/Card";
+import { MyLogo } from "./additional/Img/svg";
+import BigTitle from "./additional/BigTitle";
 
 const Box = styled.div`
-  width:100vw;
-  height:100vh;
-  position:relative;
-  overflow:hidden;
-  background:${props=>props.theme.body};
-`
-const Card = styled.div`
-	width:250px;
-	height:400px;
+  height:550vh;
+  position: relative;
+  display:flex;
+  align-items:center;
+  background: ${(props) => props.theme.body};
+`;
+const Main = styled.div`
+  position: fixed;
+  top: 15rem;
+  left: calc(10rem + 15vw);
+  height: 40vh;
+  display: flex;
+  z-index:3;
+  img {
+    height: 100%;
+  }
+`;
 
-	img{
-		width:100%;
-		height:100%;
-	}
+const Rotate = styled.div`
+	position:fixed;
+	display:block;
+	right:1rem;
+	bottom:1rem;
+	width:80px;
+	height:80px;
+	z-index:3;
 `
 const Works = () => {
-  return <ThemeProvider theme={darkTheme}>
-    <Box>
-      <Logo theme='darkTheme'/>
-      <SocialMedia theme='darkTheme'/>
-      <Button/>
+  const ref = useRef(null);
+	const myLogo = useRef(null)
 
-        <Card>
-            <img src={card} alt='cardimage'/>
-        </Card>
-    </Box>
-  </ThemeProvider>;
+  useEffect(() => {
+    let element = ref.current;
+	
+
+    const rotate = () => {
+      element.style.transform = `translateX(${-window.pageYOffset}px)`;
+			myLogo.current.style.transform = `rotate(` + -window.pageYOffset + `deg)`
+    };
+
+    window.addEventListener("scroll", rotate);
+
+    return () => window.removeEventListener("scroll", rotate);
+  }, []);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box>
+        <Logo theme="darkTheme" />
+        <SocialMedia theme="darkTheme" />
+        <Button />
+        <Main ref={ref}>
+          {WorkData.map((item) => {
+            return <Card key={item.id} data={item} />;
+          })}
+        </Main>
+				<Rotate ref={myLogo}>
+					<MyLogo width={80} height={80} fill={darkTheme.text}/>
+				</Rotate>
+				<BigTitle text="MY WORKS" top='15%' right='20%' />
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default Works;
