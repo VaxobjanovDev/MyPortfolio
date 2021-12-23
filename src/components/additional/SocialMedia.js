7f9d38f66d0d4c47ffeb393d5b5fc1d8449aed9a
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-12-23 23:27:21 +0500

    Added some styles and changes

--- src/components/additional/SocialMedia.js
+++ src/components/additional/SocialMedia.js
@@ -1,28 +1,56 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import { Icons } from "./styles/Icons";
-import { Lines } from "./styles/Lines";
 import { GitHub, Instagram, Telegram, YouTube } from "./Img/svg";
 import { darkTheme } from "../../styles/Theme.styled";
+import styled from "styled-components";
+
+const Icons = styled.div`
+  diplay: flex !important;
+  flex-direction: column;
+  align-items: center;
+
+  position: fixed;
+  bottom: 0rem;
+  left: 2rem;
+
+  z-index: 5;
+
+  & > *:not(:last-child) {
+    margin: 0.8rem 0;
+  }
+`;
+
+const Lines = styled.div`
+  margin: 0 auto;
+  width: 2px;
+  height: 8rem;
+  background-color: ${(props) =>
+    props.color === "darkTheme" ? darkTheme.text : darkTheme.body};
+`;
 
 const SocialMedia = (props) => {
   return (
     <Icons>
       <div>
-        <Link target="_blank" to="https://github.com/VaxobjanovDev">
+        <Link
+          onClick={() => {
+            window.open("https://github.com/VaxobjanovDev");
+          }}
+          to="/"
+        >
           <GitHub
             width={30}
             height={30}
             fill={props.theme === "darkTheme" ? darkTheme.text : darkTheme.body}
-						
           />
         </Link>
       </div>
       <div>
         <NavLink
-          aria-current="false"
-          target="_blank"
-          to={{ pathname: "https://t.me/VaxobjanovDev" }}
+          onClick={() => {
+            window.open("https://t.me/VaxobjanovDev");
+          }}
+          to="/"
         >
           <Telegram
             width={30}
@@ -50,7 +78,7 @@ const SocialMedia = (props) => {
         </NavLink>
       </div>
 
-      <Lines color={props.theme}/>
+      <Lines color={props.theme} />
     </Icons>
   );
 };
