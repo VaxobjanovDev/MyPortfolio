eb7d4b682fb16edee9ccbdbb5f01291fb6bacfa0
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-12-23 23:26:44 +0500

    Added some styles

--- src/components/additional/Logo.js
+++ src/components/additional/Logo.js
@@ -1,7 +1,32 @@
 import React from "react";
-import { LogoStyle } from "./styles/Logo.styled";
-const Logo = (props) => {
-  return <LogoStyle color={props.theme}>VaxobjanovDev</LogoStyle>;
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { darkTheme } from "../../styles/Theme.styled";
+
+const LogoStyle = styled.h1`
+  display: inline-block;
+  font-family: "Indie Flower", cursive;
+  font-weight: 800;
+  font-size: 3rem;
+
+  corsor: pointer;
+  position: fixed;
+  left: 2rem;
+  top: 2rem;
+  z-index: 3;
+`;
+const Link = styled(NavLink)`
+  text-decoration: none;
+  color: ${(props) =>
+    props.color === "darkTheme" ? darkTheme.text : darkTheme.body};
+`;
+
+const Logo = ({ theme }) => {
+  return (
+    <LogoStyle>
+      <Link color={theme} to="/">VaxobjanovDev</Link>
+    </LogoStyle>
+  );
 };
 
 export default Logo;
