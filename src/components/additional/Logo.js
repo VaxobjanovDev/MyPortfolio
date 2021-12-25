11d8dfd8758554b23b6aa89242355c42b2c0539f
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-12-25 23:03:27 +0500

    Added media query

--- src/components/additional/Logo.js
+++ src/components/additional/Logo.js
@@ -1,30 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import { darkTheme } from "../../styles/Theme.styled";
 
 const LogoStyle = styled.h1`
   display: inline-block;
   font-family: "Indie Flower", cursive;
   font-weight: 800;
   font-size: 3rem;
-
   corsor: pointer;
   position: fixed;
   left: 2rem;
   top: 2rem;
   z-index: 3;
+
+  @media screen and (max-width:576px){
+    font-size: calc(1.2rem + 0.5vw);
+    top: 2.2rem;
+    left: 1rem; 
+  }
 `;
 const Link = styled(NavLink)`
   text-decoration: none;
-  color: ${(props) =>
-    props.color === "darkTheme" ? darkTheme.text : darkTheme.body};
+  color: ${props=>props.theme.text};
 `;
 
-const Logo = ({ theme }) => {
+const Logo = () => {
   return (
     <LogoStyle>
-      <Link color={theme} to="/">VaxobjanovDev</Link>
+      <Link to="/">ReactDev</Link>
     </LogoStyle>
   );
 };
