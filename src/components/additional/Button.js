d472ef2007551b1077043f2f153dfc75da8d8e59
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-12-25 23:02:48 +0500

    Fixed global theme

--- src/components/additional/Button.js
+++ src/components/additional/Button.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styled, { css, keyframes } from "styled-components";
-import { NavLink } from "react-router-dom";
 import { ButtonSvg } from "./Img/svg";
 
 const rotate = keyframes`
@@ -27,7 +26,14 @@ const Main = styled.div`
   top: 2rem;
   z-index: 5;
   & > *:nth-child(1) {
-    color: ${(props) => props.theme.body};
+    color: ${(props) => props.theme.text};
+  }
+  & > *:nth-child(3) {
+    color: ${(props) => props.theme.text};
+  }
+  @media screen and (max-width:576px){
+    top:1.2rem;
+    left: 50%;  
   }
 `;
 const ButtonStyle = styled.button`
@@ -36,13 +42,17 @@ const ButtonStyle = styled.button`
   border-radius: 50%;
   width: 3.5rem;
   height: 3.5rem;
-  background-color: ${(props) => props.theme.body};
+  color: ${(props) => props.theme.text};
+  border: 2px solid ${(props) => props.click && props.theme.text};
+  background-color: ${(props) =>
+    props.click ? props.theme.body : props.theme.body};
   transform: rotate(${(props) => (props.click ? "120deg" : "-120deg")});
+  @media screen and (max-width:576px){
+    width: 2.5rem;
+    height: 2.5rem;
+  }
 `;
 
-const Link = styled(NavLink)`
-  text-decoration: none;
-`;
 const Rotate = styled.div`
   animation: ${(props) =>
       props.click
@@ -63,9 +73,7 @@ const Button = () => {
       <span>Off</span>
       <ButtonStyle click={click}>
         <Rotate onClick={() => setClick(!click)} click={click}>
-          <Link to="/">
-            <ButtonSvg height={50} width={50} />
-          </Link>
+          <ButtonSvg height={35} width={35} fill="currentColor" />
         </Rotate>
       </ButtonStyle>
       <span>On</span>
