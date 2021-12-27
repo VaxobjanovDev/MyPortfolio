6a8f5d83865ac6816ff00ed151ccad1047634b65
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-12-27 16:20:50 +0500

    Exported file from components to App

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Main } from "./Main";
+export { default as About } from "./About";
+export { default as Blog } from "./Blog";
+export { default as Works } from "./Works";
+export { default as Skills } from "./Skills";
+export { default as Sound } from "./additional/Sound";
+export { default as Button } from "./additional/Button";
