82adc44274c84c6671ccb3828cf18ccf41f3495c
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-12-27 16:21:08 +0500

    Added framer motion

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,7 +22,7 @@ const Main = () => {
   return (
     <MainContainer>
       <Container>
-        <Logo />
+        <Logo click={click} />
         <SocialMedia click={click} />
         <DarkContainer click={click} />
         <Center click={click}>
@@ -34,29 +34,90 @@ const Main = () => {
           />
           <span>Click Me</span>
         </Center>
-        <Contact to={{ pathname: "asda" }}>
-          <motion.h3 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+        <Contact
+          click={click}
+          to="/"
+          onClick={() => {
+            window.open("https://t.me/VaxobjanovDev");
+          }}
+        >
+          <motion.h3
+            initial={{
+              y: -200,
+              transition: { type: "spring", duration: 1.5, delay: 0.5 },
+            }}
+            animate={{
+              y: 0,
+              transition: { type: "spring", duration: 1.5, delay: 0.5 },
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.8 }}
+          >
             Write me...
           </motion.h3>
         </Contact>
-        <Blog to="/blog">
-          <motion.h3 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+        <Blog click={click} to="/blog">
+          <motion.h3
+            initial={{
+              y: -200,
+              transition: { type: "spring", duration: 1.5, delay: 0.5 },
+            }}
+            animate={{
+              y: 0,
+              transition: { type: "spring", duration: 1.5, delay: 0.5 },
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.8 }}
+          >
             Blog
           </motion.h3>
         </Blog>
         <Work to="/works" click={click}>
-          <motion.h3 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+          <motion.h3
+            initial={{
+              y: -200,
+              transition: { type: "spring", duration: 1.5, delay: 0.5 },
+            }}
+            animate={{
+              y: 0,
+              transition: { type: "spring", duration: 1.5, delay: 0.5 },
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.8 }}
+          >
             Work
           </motion.h3>
         </Work>
-        <BottomBar>
-          <About to="/about" click={click}>
-            <motion.h3 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+        <BottomBar click={click}>
+          <About to="/about">
+            <motion.h3
+              initial={{
+                y: 200,
+                transition: { type: "spring", duration: 1.5, delay: 0.5 },
+              }}
+              animate={{
+                y: 0,
+                transition: { type: "spring", duration: 1.5, delay: 0.5 },
+              }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.8 }}
+            >
               About
             </motion.h3>
           </About>
           <Skills to="/skills">
-            <motion.h3 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+            <motion.h3
+              initial={{
+                y: 200,
+                transition: { type: "spring", duration: 1.5, delay: 0.5 },
+              }}
+              animate={{
+                y: 0,
+                transition: { type: "spring", duration: 1.5, delay: 0.5 },
+              }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.8 }}
+            >
               My Skills
             </motion.h3>
           </Skills>
