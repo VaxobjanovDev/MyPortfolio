9f17ffc8763b5c387e49ee3271596b66ac42686e
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-12-27 16:19:26 +0500

    Added media query and Added Framer Motion

--- src/components/About.js
+++ src/components/About.js
@@ -4,77 +4,107 @@ import Logo from "./additional/Logo";
 import PacrticleComponent from "./additional/PacrticleComponent";
 import SocialMedia from "./additional/SocialMedia";
 
-import spaceman from './assets/spaceman.png'
+import spaceman from "./assets/spaceman.png";
 import BigTitle from "./additional/BigTitle";
+import { motion } from "framer-motion";
 
 const Box = styled.div`
-  width:100vw;
-  height:100vh;
-  position:relative;
-  overflow:hidden;
-  background:${props=>props.theme.body};
-`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+  background: ${(props) => props.theme.body};
+`;
 const float = keyframes`
   0%{transform:translateY(-10px)}
   50%{transform:translateY(15px) translateX(15px)}
   100%{transform:translateY(-10px)}
-`
+`;
 const SpaceMan = styled.div`
-  position:absolute;
-  top:10%;
-  right:5%;
-  width:20vw;
+  position: absolute;
+  top: 10%;
+  right: 5%;
+  width: 20vw;
   animation: ${float} ease 4s infinite;
-  img{
-    width:100%;
-    height:auto;
+  img {
+    width: 100%;
+    height: auto;
   }
-`
-const Main = styled.div`
-  border:2px solid ${props=>props.theme.text};
-  color:${props=>props.theme.text};
-  padding:2rem;
-  width:50vw;
-  height:60vh;
-  z-index:3;
 
-  display:flex;
-  flex-direction:column;
-  justify-content:space-evenly;
-  align-items:center;
-   
-  backdrop-filter:blur(5px);
+  @media screen and (max-width: 576px) {
+    width: 50vw;
+    right: 0%;
+  }
+`;
+const Main = styled(motion.div)`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 50vw;
+  height: 60vh;
+  z-index: 3;
 
-  position:absolute;
-  left:calc(5rem + 5vw);
-  top:12rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
 
-  font-size:calc(0.6rem + 1vw);
-  font-style:italic;
-  font-family: "Ubuntu Mono", monospace; 
-  font-weight:400;
-  line-height:1.5;
-`
+  backdrop-filter: blur(5px);
 
-const About = ({theme}) => {
+  position: absolute;
+  left: calc(5rem + 5vw);
+  top: 12rem;
+
+  font-size: calc(0.6rem + 1vw);
+  font-style: italic;
+  font-family: "Ubuntu Mono", monospace;
+  font-weight: 400;
+  line-height: 1.5;
+
+  @media screen and (max-width: 576px) {
+    top: 10rem;
+    left: calc(3rem + 3vw);
+  }
+`;
+
+const About = ({ theme }) => {
   return (
     <Box>
-      <Logo/>
-      <SocialMedia/>
-      <PacrticleComponent theme={theme}/>
+      <Logo />
+      <SocialMedia />
+      <PacrticleComponent theme={theme} />
 
       <SpaceMan>
-        <img src={spaceman} alt="spaceman"/>
+        <img src={spaceman} alt="spaceman" />
       </SpaceMan>
 
-      <Main>
-        <h3>I'm a front-end developer in Namangan. I can make simple and beatiful website and other useful tools.</h3>
-        <h3>I'm interested in Back-end field also. So I'm learning Node js and Express js nowadays.</h3>
-        <h3>My law is to the end of learning everything. Also It also doesn’t matter if the growth is minus or plus, the important thing is that it’s done</h3>
+      <Main
+        initial={{
+          y: 200,
+          transition: { type: "spring", duration: 1.5, delay: 0.5 },
+        }}
+        animate={{
+          y: 0,
+          transition: { type: "spring", duration: 1.5, delay: 0.5 },
+        }}
+      >
+        <h3>
+          I'm a front-end developer in Namangan. I can make simple and beatiful
+          website and other useful tools.
+        </h3>
+        <h3>
+          I'm interested in Back-end field also. So I'm learning Node js and
+          Express js nowadays.
+        </h3>
+        <h3>
+          My law is to the end of learning everything. Also It also doesn’t
+          matter if the growth is minus or plus, the important thing is that
+          it’s done
+        </h3>
       </Main>
-      <BigTitle text="ABOUT" top="10%" left="5%"/>
+      <BigTitle text="ABOUT" top="10%" left="5%" />
     </Box>
-  )
+  );
 };
 
 export default About;
